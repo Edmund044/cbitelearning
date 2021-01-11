f1409dac0d0994bbd285e6547b3915a1abaddd52
Edmund044 <edmundopiyo@gmail.com>
2021-01-11 10:35:42 +0300

    modified:   src/app/app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CertificatesComponent } from './certificates/certificates.component';
 import { MywalletComponent } from './mywallet/mywallet.component';
+import { VideocardComponent } from './videocard/videocard.component';
 const routes: Routes = [
   {path: '', redirectTo:'/home', pathMatch:'full'},
   {path: 'home', component: HomeComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'certificates', component: CertificatesComponent},
-  {path: 'mywallet', component: MywalletComponent}
+  {path: 'mywallet', component: MywalletComponent},
+  {path: 'course', component:  VideocardComponent}
+
 ];
 
 @NgModule({
